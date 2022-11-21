7a04a3dcbc910ceeeca6f3441e6611ccdb1dfde2
Alexey-beercode <alsemkovbn@gmail.com>
2022-11-21 22:58:10 +0300

    Update mainpage

--- MAUILearn/MAUILearn/MainPage.xaml.cs
+++ MAUILearn/MAUILearn/MainPage.xaml.cs
@@ -13,10 +13,7 @@ public partial class MainPage : ContentPage
 	{
 		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		CounterBtn.Text = $"Нажато {count} раз";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
